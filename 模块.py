200a4289fef8ec59c24d4a6926138b3e9aa6ef80
23147906 <32630947+23147906@users.noreply.github.com>
2018-03-07 23:37:42 +0800

    Add files via upload

---
+++ 模块.py
@@ -0,0 +1,24 @@
+#Author:Win_Li   23147
+#Date:2018-01-09 21:48
+
+import hashlib
+
+# import logging
+#
+# logger = logging.getLogger() #创建logger对象
+#
+# fh = logging.FileHandler('test.log') #创建文件对象
+#
+# ch = logging.StreamHandler() #创建屏幕对象
+#
+# formatter = logging.Formatter('%(asctime)s -')#输出格式对象
+#
+# fh.setFormatter(formatter) #文件获取格式对象
+# ch.setFormatter(formatter)#屏幕获取格式对象
+# # logger 获取文件和屏幕对象
+# logger.addHandler(fh)
+# logger.addHandler(ch)
+
+import re
+
+re.findall('w\w')
